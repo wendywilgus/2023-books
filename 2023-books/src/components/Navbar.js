6468cf272d3049e4c3bdd12b9eec38c3d5d53bd2
Wendy Wilgus <wilguswendy@gmail.com>
2023-12-27 11:23:24 -0500

    deletions and images

---
+++ 2023-books/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <>
+      <nav className='navbar'>
+      <div className='navbar-container'>
+        <Link to="/" className='navbar-logo'>
+          2023 Book List <i class="fa-solid fa-book" />
+        </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/full-list' className='nav-links' onClick={closeMobileMenu}>
+              Full List
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </>
+  );
+}
+
+export default Navbar;
