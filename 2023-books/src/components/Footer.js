f811331fd2b3abb1af53a307318befc9ca84dfeb
Wendy Wilgus <wilguswendy@gmail.com>
2024-01-06 10:51:30 -0500

    Footer Additions

--- 2023-books/src/components/Footer.js
+++ 2023-books/src/components/Footer.js
@@ -20,6 +20,77 @@ function Footer() {
                 </form>
             </div>
         </section>
+        <div className='footer-links'>
+            <div className='footer-link-wrapper'>
+                <div className='footer-link-items'>
+                    <Link to='/'>
+                        Home
+                    </Link>
+                    <Link to='/bookshelf'>
+                        Monthly Bookshelf
+                    </Link>
+                    <Link to='/favorites'>
+                        My Favorites
+                    </Link>
+                    <Link to='/full-list'>
+                        Full List
+                    </Link>
+                </div>
+            </div>
+            <div className='footer-link-items'>
+                <small class='website-rights'>Wendy Wilgus © 2024</small>
+                <small >
+                    <Link
+                        to='https://wendywilgus.github.io/Portfolio_in_React/'
+                        target='_blank'
+                        aria-label='Portfolio'
+                        class='website-rights'
+                        >View My Portfolio
+                        </Link>
+                </small>
+            </div>
+            <section class='social-media'>
+        <div class='social-media-wrap'>
+          
+          <div class='social-icons'>
+            <Link
+              class='social-icon-link facebook'
+              to='https://www.facebook.com/wendy.a.wilgus'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i class='fab fa-facebook-f' />
+            </Link>
+            <Link
+              class='social-icon-link instagram'
+              to='https://www.instagram.com/bear__and__bee/?hl=en'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <i class='fab fa-instagram' />
+            </Link>
+            <Link
+              class='social-icon-link linkedin'
+              to='https://www.linkedin.com/in/wendy-wilgus-750b00a1/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i class='fa-brands fa-linkedin' />
+            </Link>
+            <Link
+              class='social-icon-link github'
+              to='https://github.com/wendywilgus'
+              target='_blank'
+              aria-label='Github'
+            >
+              <i class="fa-brands fa-square-github"></i>
+            </Link>
+            
+          </div>
+        </div>
+      </section>
+        </div>
+        
     </div>
   )
 }
